80a21f27a49dcab9a5cf17f1466e778cfaa603f0
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-10-13 21:26:52 +1300

    footer done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { Switch, Route } from 'react-router-dom'
+import './App.css'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Footer />
     </>
   )
 }
